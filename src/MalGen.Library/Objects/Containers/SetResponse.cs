3c295df9a13f9e736b8def5fa00fbd0ab72a7094
Jarred Capellman <jcapellman@hotmail.com>
2017-06-11 21:58:42 -0500

    Added: SetResponse Container Class

---
+++ src/MalGen.Library/Objects/Containers/SetResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MalGen.Library.Objects.Containers
+{
+    public class SetResponse<T>
+    {
+        public T ObjectValue { get; set; }
+
+        public string ExceptionString { get; set; }
+
+        public bool HasError => !string.IsNullOrEmpty(ExceptionString);
+
+        public SetResponse(T objectValue)
+        {
+            ObjectValue = objectValue;
+        }
+
+        public SetResponse(Exception ex)
+        {
+            ExceptionString = ex.ToString();
+        }
+    }
+}
